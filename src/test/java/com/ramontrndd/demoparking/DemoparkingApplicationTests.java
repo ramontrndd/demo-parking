7af4c9ca4ba619d48fce4a07d939079cd57e2282
Ramon Trindade <4924036@alunouninter.com>
2024-12-18 15:12:45 -0300

    first commit

---
+++ src/test/java/com/ramontrndd/demoparking/DemoparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramontrndd.demoparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
